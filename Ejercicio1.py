5e3c4705921136b80eaadbccab9870ffa342e509
Alexander Giraldo Poveda <alexander.giraldo@mvm.com.co>
2021-03-23 14:25:13 -0500

    Finaliza ejercicio 1

--- Ejercicio1.py
+++ Ejercicio1.py
@@ -29,3 +29,16 @@ mostrarLista(numerosLista)
 #Mostrar su longitd
 print("*********LONGITUD*********************")
 print("Logitud de la Lista es: " + str(len(numerosLista)))
+
+#Buscar un numero
+print("*********BUSCAR EN LA LISTA***********")
+busqueda = int(input("Introduce el numero: "))
+
+comprobar = isinstance(busqueda, int)
+while not comprobar or busqueda <=0:
+    busqueda = int(input("Introduce el numero: "))
+else:
+    print(f"Has introducido el {busqueda}")
+
+search = numerosLista.index(busqueda)
+print(f"El número existe en el indice: {search}")
